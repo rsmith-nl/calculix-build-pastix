1d2940faebc4f90015268c4e89fa05c4d5a9fcac
Roland Smith <rsmith@xs4all.nl>
2024-04-23 21:14:56 +0200

    Use complete environment when installing, just to be sure..

--- 02_build_openblas.sh
+++ 02_build_openblas.sh
@@ -5,7 +5,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2024-02-18T16:43:42+0100
-# Last modified: 2024-03-24T14:58:55+0100
+# Last modified: 2024-04-23T21:13:19+0200
 
 set -e
 
@@ -31,6 +31,6 @@ env CC=gcc13 FC=gfortran13 AR=gcc-ar13 \
 #  Fortran compiler ... GFORTRAN  (cmd & version : GNU Fortran (FreeBSD Ports Collection) 12.2.0)
 #  Library Name     ... libopenblas_haswell-r0.3.26.a (Single-threading)
 
-env PREFIX=/zstorage/home/rsmith/tmp/src/calculix-build NO_SHARED=1 USE_THREAD=0 gmake install
+env PREFIX=/zstorage/home/rsmith/tmp/src/calculix-build NO_SHARED=1 INTERFACE64=1 BINARY=64 USE_THREAD=0 gmake install
 cd ..
 rm -rf OpenBLAS
