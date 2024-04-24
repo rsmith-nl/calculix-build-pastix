2ddce4b002690857e6d1f22030c9631bebedbc46
Roland Smith <rsmith@xs4all.nl>
2024-04-24 12:48:37 +0200

    Enable locking and increase buffer size for OpenBLAS.

--- 02_build_openblas.sh
+++ 02_build_openblas.sh
@@ -5,7 +5,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2024-02-18T16:43:42+0100
-# Last modified: 2024-04-23T21:13:19+0200
+# Last modified: 2024-04-24T12:09:41+0200
 
 set -e
 
@@ -16,8 +16,10 @@ mv OpenBLAS-0.3.26 OpenBLAS
 
 cd OpenBLAS
 # Do *not* use USE_OPENMP=1. Pastix needs a singe-threaded build, hence USE_THREAD=0.
+# Enable locking in case BLAS routines are called in a multithreaded program.
 env CC=gcc13 FC=gfortran13 AR=gcc-ar13 \
-    NO_SHARED=1 INTERFACE64=1 BINARY=64 USE_THREAD=0 \
+    PREFIX=/zstorage/home/rsmith/tmp/src/calculix-build \
+    NO_SHARED=1 INTERFACE64=1 BINARY=64 USE_THREAD=0 BUFFERSIZE=25 USE_LOCKING=1 \
     gmake
 
 
@@ -31,6 +33,9 @@ env CC=gcc13 FC=gfortran13 AR=gcc-ar13 \
 #  Fortran compiler ... GFORTRAN  (cmd & version : GNU Fortran (FreeBSD Ports Collection) 12.2.0)
 #  Library Name     ... libopenblas_haswell-r0.3.26.a (Single-threading)
 
-env PREFIX=/zstorage/home/rsmith/tmp/src/calculix-build NO_SHARED=1 INTERFACE64=1 BINARY=64 USE_THREAD=0 gmake install
+env CC=gcc13 FC=gfortran13 AR=gcc-ar13 \
+    PREFIX=/zstorage/home/rsmith/tmp/src/calculix-build \
+    NO_SHARED=1 INTERFACE64=1 BINARY=64 USE_THREAD=0 BUFFERSIZE=25 USE_LOCKING=1 \
+    gmake install
 cd ..
 rm -rf OpenBLAS
