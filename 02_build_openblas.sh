010c989d3a59a4a49de1b50525efbe8d940aa24e
Roland Smith <rsmith@xs4all.nl>
2024-06-07 13:22:31 +0200

    Remove comments.

--- 02_build_openblas.sh
+++ 02_build_openblas.sh
@@ -5,7 +5,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2024-02-18T16:43:42+0100
-# Last modified: 2024-04-24T14:11:39+0200
+# Last modified: 2024-06-07T13:22:13+0200
 
 set -e
 PREFIX=`pwd`
@@ -26,17 +26,6 @@ env CC=gcc13 FC=gfortran13 AR=gcc-ar13 \
     NO_SHARED=1 INTERFACE64=1 BINARY=64 USE_THREAD=0 \
     BUFFERSIZE=25 USE_LOCKING=1 DYNAMIC_ARCH=1 \
     gmake
-
-
-# OpenBLAS build complete. (BLAS CBLAS LAPACK LAPACKE)
-#
-#  OS               ... FreeBSD
-#  Architecture     ... x86_64
-#  BINARY           ... 64bit
-#  Use 64 bits int    (equivalent to "-i8" in Fortran)
-#  C compiler       ... GCC  (cmd & version : gcc13 (FreeBSD Ports Collection) 12.2.0)
-#  Fortran compiler ... GFORTRAN  (cmd & version : GNU Fortran (FreeBSD Ports Collection) 12.2.0)
-#  Library Name     ... libopenblas_haswell-r0.3.26.a (Single-threading)
 
 env CC=gcc13 FC=gfortran13 AR=gcc-ar13 \
     PREFIX=${PREFIX} \
